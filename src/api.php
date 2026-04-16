33e1a1aa8e0b08c8fa955c72f4ae807ddd203e84
pyrororoo <157348093+pyrororoo@users.noreply.github.com>
2026-04-16 21:48:39 +0800

    Added: Login function and explanatory comments

--- src/api.php
+++ src/api.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json");
 
 require "db.php";
 
-// Only POST allowed
+// Accepts POST requests only
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     respond(405, "error", "Method Not Allowed. Use POST request");
 }
@@ -16,7 +16,7 @@ $action = $_POST['action'] ?? '';
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 
-//
+//general response function; reusable accross functions
 function respond($http_code, $status, $message, $data = null) {
     http_response_code($http_code);
     echo json_encode([
@@ -27,7 +27,8 @@ function respond($http_code, $status, $message, $data = null) {
     exit;
 }
 
-// REGISTER 
+// REGISTER function
+//accepts username and password, checks if username exists, if not creates new user with hashed password
 if ($action === "register") {
     if (!$username || !$password)
         respond(400, "error", "Please fill all fields");
@@ -50,7 +51,34 @@ if ($action === "register") {
         : respond(500, "error", "Registration failed");
 }
 
+// LOGIN
+// validates username and password, checks if user exists, if yes verifies password and responds accordingly
+elseif ($action === "login") {
+    if (!$username || !$password)
+        respond(400, "error", "Please fill all fields");
+
+    $stmt = $conn->prepare("SELECT id,password FROM users WHERE username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $res = $stmt->get_result();
+
+    if ($res->num_rows == 0)
+        respond(404, "error", "User not found");
+
+    $user = $res->fetch_assoc();
+
+    if (password_verify($password, $user['password'])) {
+        respond(200, "success", "Login successful", [
+            "user_id" => $user['id'],
+            "username" => $username
+        ]);
+    } else {
+        respond(401, "error", "Incorrect password");
+    }
+}
+
 // INVALID
+// if action is not valid i.e. does not exist, respond with error
 else {
     respond(400, "error", "Invalid request action or action not yet implemented");
 }
